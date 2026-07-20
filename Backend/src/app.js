8bf58ae6e92ae16ae18ce8f62916aae91ccaadac
Utkarsh1-ux <utkarshramu53@gmail.com>
2026-07-20 21:32:42 +0530

    ai integrated

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -13,10 +13,12 @@ app.use(cors({
 
 // Require all the routes here
 const authRouter = require("./routes/auth.route.js")
+const interviewRouter = require("./routes/interview.routes.js")
 
 
 // using all the routes here
 app.use("/api/auth" , authRouter)
+app.use("/api/interview" , interviewRouter)
 
 
 module.exports = app
